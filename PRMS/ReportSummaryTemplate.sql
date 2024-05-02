09c43d261bf375004a28bfeffbb420c960ddad05
Rogh ian <RoghI@DESKTOP-1LPIHIN>
2024-05-02 11:00:34 +0800

    updated sql report summary template

--- PRMS/ReportSummaryTemplate.sql
+++ PRMS/ReportSummaryTemplate.sql
@@ -9,13 +9,13 @@ SELECT
       ,[HasParticipated]
       ,[ReportDate]
 FROM [publishers-record-db].[dbo].[PioneerReport]
-WHERE month(ReportDate) = month('03/13/2024') and year(ReportDate) = year('03/13/2024')
+WHERE month(ReportDate) = month('04/13/2024') and year(ReportDate) = year('04/13/2024')
 --TOTAL
 SELECT
 		SUM(CAST([publishers-record-db].[dbo].[PioneerReport].[Hours] AS INT) ) TotalHours,
 		SUM(BibleStudies) TotalBibleStudies
 FROM [publishers-record-db].[dbo].[PioneerReport]
-WHERE month(ReportDate) = month('03/13/2024') and year(ReportDate) = year('03/13/2024')
+WHERE month(ReportDate) = month('04/13/2024') and year(ReportDate) = year('04/13/2024')
 
 ------------------------------------------------------------------------------------------
 
@@ -29,13 +29,13 @@ SELECT
       ,[BibleStudies]
       ,[ReportDate]
 FROM [publishers-record-db].[dbo].[AuxiReport]
-WHERE month(ReportDate) = month('03/13/2024') and year(ReportDate) = year('03/13/2024')
+WHERE month(ReportDate) = month('04/13/2024') and year(ReportDate) = year('04/13/2024')
 --TOTAL
 SELECT
 		SUM(CAST([publishers-record-db].[dbo].[AuxiReport].[Hours] AS INT) ) TotalHours,
 		SUM(BibleStudies) TotalBibleStudies
 FROM [publishers-record-db].[dbo].[AuxiReport]
-WHERE month(ReportDate) = month('03/13/2024') and year(ReportDate) = year('03/13/2024')
+WHERE month(ReportDate) = month('04/13/2024') and year(ReportDate) = year('04/13/2024')
 
 ------------------------------------------------------------------------------------------
 
@@ -50,12 +50,12 @@ SELECT
       ,[Appointed]
       ,[ReportDate]
 FROM [publishers-record-db].[dbo].[PubAndUnPubReport]
-WHERE month(ReportDate) = month('03/13/2024') and year(ReportDate) = year('03/13/2024')
+WHERE month(ReportDate) = month('04/13/2024') and year(ReportDate) = year('04/13/2024')
 --TOTAL
 SELECT
 		SUM(BibleStudies) TotalBibleStudies
 FROM [publishers-record-db].[dbo].[PubAndUnPubReport]
-WHERE month(ReportDate) = month('03/13/2024') and year(ReportDate) = year('03/13/2024')
+WHERE month(ReportDate) = month('04/13/2024') and year(ReportDate) = year('04/13/2024')
 
 ------------------------------------------------------------------------------------------
 
@@ -67,15 +67,29 @@ SELECT
       ,[GroupId]
       ,[ReportDate]
 FROM [publishers-record-db].[dbo].[PublisherWithoutReport]
-WHERE month(ReportDate) = month('03/13/2024') and year(ReportDate) = year('03/13/2024')
+WHERE month(ReportDate) = month('04/13/2024') and year(ReportDate) = year('04/13/2024')
 
 ------------------------------------------------------------------------------------------
 
 --TOTAL NUMBER OF REPORTING
 SELECT COUNT(*) AS TOTAL_NUMBER_OF_REPORTING
 FROM [publishers-record-db].[dbo].Reports
-WHERE month(ReportDate) = month('03/13/2024') and year(ReportDate) = year('03/13/2024')
+WHERE month(ReportDate) = month('04/13/2024') and year(ReportDate) = year('04/13/2024')
 AND
 Id IS NOT NULL
 AND 
 Reports.HasParticipated = 1
+
+------------------------------------------------------------------------------------------
+
+--PUBLISHERS THAT HAS NOT REPORTED YET
+SELECT 
+ROW_NUMBER() OVER (ORDER BY GroupId, LastName, FirstName DESC) AS 'Count',
+Publishers.Id, 
+FirstName, 
+LastName, 
+GroupId
+FROM Publishers
+  WHERE 1 = 1 
+  AND IsActive = 1
+  AND Id NOT IN (SELECT Reports.PublisherId FROM Reports WHERE month(ReportDate) = month('04/13/2024') and year(ReportDate) = year('04/13/2024'))
